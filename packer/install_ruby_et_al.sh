d6bc1ec3d686409b83990728310a7138095cbbc9
Chase Southard <chase.southard@gmail.com>
2019-10-27 12:55:00 -0400

    can't seem to get this to work correctly. apt lock problems.

---
+++ packer/install_ruby_et_al.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get -y update
+
+sudo apt-get -fyq install gcc g++ make git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+cd ~
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+source ~/.bashrc
+
+rbenv install 2.6.5
+rbenv global 2.6.5
+ruby -v
+
+gem install bundler
+
+git config --global color.ui true
+git config --global user.name "Chase Southard"
+git config --global user.email "chase.southard@gmail.com"
+
+sudo apt-get install -y postgresql libpq-dev
+
+sudo -u postgres createuser ubuntu -s
